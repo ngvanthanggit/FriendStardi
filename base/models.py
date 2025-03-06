ff0bb025223f2441719dbdddef6f78037bdc11f3
ngvanthanggit <thangitcbg@gmail.com>
2025-03-07 03:01:38 +0800

    refactor: superuser creation management

--- base/models.py
+++ base/models.py
@@ -1,6 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 # Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_superuser(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
@@ -9,7 +21,9 @@ class User(AbstractUser):
     avatar = models.ImageField(null=True, default="avatar_new.png")
     
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
+    
+    objects = CustomUserManager()
     
     def __str__(self):
         return self.username
